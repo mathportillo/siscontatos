beafb3ebefff3d4c1b83dcef1432abe970ed7466
cvmagal <cvmagal@0345a1ad-108e-495e-8c33-cb5e34f35e8a>
2010-09-27 18:52:06 +0000

    rodape

--- system/application/views/_template_close_view.php
+++ system/application/views/_template_close_view.php
@@ -1,7 +1,14 @@
 			</div>
 			<div id="div_footer">
+				<br />
+				<br />
 				Copyright &copy; 2010 - <?= anchor('util/licenca','GNU GPL License')?>
-			</div>
+				<br />
+				<div style="text-align: center; width: 80%; margin: auto;"><? $this->load->view('rodape_view'); ?>
+				<br />
+				<br />
+				</div>
+			</div> 
 		</div>
 	</body>
 </html>
---
+++ system/application/views/rodape_view.php
@@ -0,0 +1,7 @@
+<b>SEPLAG - Secretaria do Planejamento e Gestão </b>
+<br />
+Av. Gal Afonso Albuquerque Lima - Edifício SEPLAG - 3ºAndar - 60.830-120 - Cambeba
+<br />
+Fortaleza/Ce - Central de Atendimento: 3101.3821 | 3101.3822 - Fax: (85) 3101.4518 
+<br />
+Governo do Estado do Ceará. Todos os direitos reservados.
